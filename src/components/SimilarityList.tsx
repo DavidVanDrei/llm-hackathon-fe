361a3a86699b3fe8dae332ad65201a77359913d1
DavidVanDrei <vandrei.1@osu.edu>
2023-02-27 00:11:09 -0500

    revert db additions

--- src/components/SimilarityList.tsx
+++ src/components/SimilarityList.tsx
@@ -4,21 +4,21 @@ import { Note } from '../interfaces/index'
 
 
 export default async function SimilarityList() {
-  const fetchNotes = async ()=> {
-    return await prisma.note.findMany()
-    }
-  const notes:Note[] = await fetchNotes()
+  // const fetchNotes = async ()=> {
+  //   return await prisma.note.findMany()
+  //   }
+  // const notes:Note[] = await fetchNotes()
 
   return (
     <ul className="menu w-56 flex-none rounded-box">
       <li className="menu-title">
         <span>Relevent Notes</span>
       </li>
-      {notes.map((note) => (
+      {/* {notes.map((note) => (
         <li key={note.id}>
           <h3>{note.title}</h3>
         </li>
-      ))}
+      ))} */}
       <li><Link href='/api/auth/logout'>Logout</Link></li>
     </ul>
   )
