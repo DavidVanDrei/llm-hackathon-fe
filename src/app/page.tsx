21f9c543a38738209cca33b22ede5b99b3546743
Lesaun Harvey <lesaun@gmail.com>
2023-02-26 17:44:16 -0800

    rollback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getUser } from "@/utils/auth"
 export default async function Home() {
   const user = await getUser()
 
-  return (user.email !== 'anonymous'
+  return (user.email !== 'anonymous' || process.env.NODE_ENV === "development"
     // @ts-expect-error Server Component
     ? <MentatMind />
     : <LandingPage />
