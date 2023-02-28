ca0c2314805704aaf03ebb4e16a65c83abdc34cb
Lesaun Harvey <lesaun@gmail.com>
2023-02-27 17:51:22 -0800

    message submit

---
+++ src/app/message/route.ts
@@ -0,0 +1,4 @@
+
+export async function POST(request: Request) {
+  return (Response as any).json({ message: 'Hello World' })
+}
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,26 @@
 'use client'
-import prisma from '../lib/prisma'
-import { Note } from '../interfaces/index'
+
 import { useState } from 'react'
 
 export default function Input() {
-  const [value,setValue] = useState("")
+	const [value, setValue] = useState("")
 
+	const handleSubmit = () => {
+		fetch('/message', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ message: value })
+		})
+			.then(response => console.log(response.json()))
+			.catch(error => console.error(error))
+	}
 
-  return (
-    <div className='w-full self-end mb-8'>
-
-      <textarea  value={value} onChange={(e) => setValue(e.target.value)} className="w-full textarea textarea-bordered"></textarea>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-
-    </div>
-
-  )
+	return (
+		<div className='w-full self-end mb-8'>
+			<textarea value={value} onChange={(e) => setValue(e.target.value)} className="w-full textarea textarea-bordered" />
+			<button type="submit" onClick={handleSubmit}>Submit</button>
+		</div>
+	)
 }
