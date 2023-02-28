e2578faf63619018889586b7c40f279bd4b8affb
DavidVanDrei <vandrei.1@osu.edu>
2023-02-27 20:06:41 -0500

    started create note component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,19 @@
 'use client'
+import prisma from '../lib/prisma'
+import { Note } from '../interfaces/index'
+import { useState } from 'react'
 
 export default function Input() {
+  const [value,setValue] = useState("")
+
+
   return (
     <div className='w-full self-end mb-8'>
-      <textarea className="w-full textarea textarea-bordered"></textarea>
+
+      <textarea  value={value} onChange={(e) => setValue(e.target.value)} className="w-full textarea textarea-bordered"></textarea>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+
     </div>
+
   )
 }
---
+++ src/pages/api/createNote.ts
@@ -0,0 +1,4 @@
+export function createNote(req,res){
+
+    console.log("HELLO, req")
+}
