b1883b0696d76b84c24e763b94ed8d8683ebf381
DavidVanDrei <vandrei.1@osu.edu>
2023-03-04 13:27:24 -0500

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,14 @@ import { getUser } from "@/utils/auth"
 export default async function Home() {
   const user = await getUser()
   const dev = process.env.NODE_ENV === "development"
+  console.log(user.email)
+
 
   return (
     <>
-      {user.email !== 'anonymous' || dev
+      <p>{user.email}</p>
+
+      {user.email !== 'anonymous' || dev 
         // @ts-expect-error Server Component
         ? <MentatMind user={dev ? { email: 'dev' } : user} />
         : <LandingPage user={user} />}
