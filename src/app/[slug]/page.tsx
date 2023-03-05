5714e6d017ba7aa344430ec4600bafd514fe3918
DavidVanDrei <vandrei.1@osu.edu>
2023-03-04 23:27:54 -0500

    mr

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { getUser } from "@/utils/auth"
 
 export default async function Home({ params: { slug } }: { params: { slug: string } }) {
   const user = await getUser()
-
+  console.log("slug",slug)
   const dev = process.env.NODE_ENV === "development"
   return (
     <>
